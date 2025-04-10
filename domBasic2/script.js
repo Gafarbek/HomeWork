b6cafd81396e55df4dd692cd83b6aff4818ebd7f
Gafarbek <abdusamatovgafarbek7@gmail.com>
2025-04-10 12:39:32 +0500

    Add files via upload

--- domBasic2/script.js
+++ domBasic2/script.js
@@ -4,19 +4,36 @@ const disks = document.querySelectorAll('.disks button')
 const values = document.querySelectorAll('.values h2')
 const move = document.querySelector('.move h2')
 const price = document.querySelector('.price h2')
+const img = document.querySelectorAll('img')
 
+
+const tesla = img[0]
+const imgDisks = img[1]
 const speed = values[0]
 const temp = values[1]
 const dataDisks = values[2]
 
-const minTemp = -91
-const maxTemp = 56
+let actualPrice = 89990
+
+
+const minTemp = -10
+const maxTemp = 40
 
 const minSpeed = 0
 const maxSpeed = 280
 
 const minDisks = 0
 const maxDisks = 24
+
+
+let angle = 0
+function rotate() {
+    angle = (angle + 1) % 360
+    imgDisks.style.transform = `rotate(${angle}deg)`
+    requestAnimationFrame(rotate)
+}
+
+rotate()
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -35,6 +52,9 @@ buttons.forEach((button) => {
             const newMove = pureMove - value * 2
             
             move.textContent = `${newMove}км`
+
+            imgDisks.style.display = 'none'
+            tesla.style.display = 'block'
         }
         
     })
@@ -51,6 +71,14 @@ parameters.forEach((degree) => {
         
         if (newTemp !== pureTemp) {
             temp.textContent = (`${newTemp}°`)
+
+            const pureTemp = parseFloat(move.textContent)
+            const newTemps = pureTemp - tempers * 2
+            
+            move.textContent = `${newTemps}км`
+
+            imgDisks.style.display = 'none'
+            tesla.style.display = 'block'
         }
     })
 })
@@ -61,11 +89,29 @@ disks.forEach((disk) => {
         const pureDisks = parseFloat(dataDisks.textContent)      
         let newDisks = pureDisks + size
         
+        imgDisks.style.display = 'block'
+        tesla.style.display = 'none'
+        
+        let width = imgDisks.clientWidth
+        let height = imgDisks.clientHeight
+
+        
         if (newDisks > maxDisks) newDisks = maxDisks
         if (newDisks < minDisks) newDisks = minDisks
         
         if (newDisks !== pureDisks) {
             dataDisks.textContent = (`${newDisks}`)
+
+            const newSize = size > 0 ? 10 : -10
+            
+            imgDisks.style.width = width + newSize + 'px'
+            imgDisks.style.height = height + newSize + 'px'
+            
+           actualPrice += size > 0 ? 1000 : -1000
+           actualPrice = Math.max(0, actualPrice)
+           let priceString = actualPrice.toString()
+
+           price.textContent =  `$${priceString.slice(0, 2)}, ${priceString.slice(2)}`
         }
     })
 })
